fa4a6bce5df364133eeeeec729fabe6dda107e62
YUBEENLIM <bgs0782@gmail.com>
2023-12-15 10:23:03 +0900

    ë로그인,로ê , 로그아웃, 글홈, 글글 목록 조회, 글 작성, 글 상세내용 조회 구현완료

---
+++ src/main/java/com/ll/medium1/domain/member/member/controller/MemberController.java
@@ -0,0 +1,55 @@
+package com.ll.medium1.domain.member.member.controller;
+
+import com.ll.medium1.domain.member.member.entity.Member;
+import com.ll.medium1.domain.member.member.service.MemberService;
+import com.ll.medium1.global.RsData.RsData;
+import com.ll.medium1.global.rq.Rq.Rq;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@Controller
+@RequestMapping("/member")
+@RequiredArgsConstructor
+public class MemberController {
+    private final MemberService memberService;
+    private final Rq rq;
+
+    @GetMapping("/join")
+    public String showJoin() {
+        return "domain/member/member/join";
+    }
+    @Setter
+    @Getter
+    public static class JoinForm {
+        @NotBlank
+        private String username;
+        @NotBlank
+        private String password;
+    }
+
+    @PostMapping("/join")
+    public String join(@Valid JoinForm joinForm) {
+        RsData<Member> joinRs = memberService.join(joinForm.getUsername(), joinForm.getPassword());
+
+        if (joinRs.isFail()) {
+            return rq.historyBack(joinRs.getMsg());
+        }
+
+        return rq.redirect(
+                "/",
+                joinRs.getMsg()
+        );
+    }
+    @GetMapping("/login")
+    public String showLogin() {
+        return "domain/member/member/login";
+    }
+}

3975d0e4b1f6ff4d2c219d0958ca91b670a84923
YUBEENLIM <bgs0782@gmail.com>
2023-12-15 09:29:20 +0900

    setting

---
+++ src/main/java/com/ll/medium1/Medium1Application.java
@@ -0,0 +1,13 @@
+package com.ll.medium1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Medium1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Medium1Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/ll/medium1/Medium1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ll.medium1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Medium1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
